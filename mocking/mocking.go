507d2cebf3ca4611723a5894eaed333e6c4ecf8e
aybangueco <203548283+aybangueco@users.noreply.github.com>
2025-11-05 13:09:23 +0800

    feat: partial mocking test completed

---
+++ mocking/mocking.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprintf(out, finalWord)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
+}
